bac1a46330f5f23742544b8281e9a0ce8d78ad26
Brutenis Gliwa <brutenis.gliwa@planet-ai.de>
2021-04-08 11:29:56 +0200

    Added xset to keyboard.sh

--- keyboard.sh
+++ keyboard.sh
@@ -3,6 +3,9 @@
 # Set us layout
 setxkbmap us
 
+# Set a more reasonable key-repeat rate
+xset r rate 200 50
+
 # Run xmodmap after setting layout (disable caps-lock, add umlauts)
 xmodmap ~/.Xmodmap
 
