d707d8ee6e12c1594b93080a6893c4caa987c06b
LiquidFun <brutenis@gmail.com>
2023-02-27 00:15:48 +0100

    Add key-up

--- keyboard.sh
+++ keyboard.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+# Reset keys which might get stuck due to barrier issue:
+# https://github.com/debauchee/barrier/issues/207#issuecomment-542744072
+xdotool keyup Shift_L Shift_R Control_L Control_R Alt_L Alt_R Super_L Super_R Hyper_L Hyper_R Caps_Lock 204 205 206 207
+
 # Set us layout
 setxkbmap us
 
 # Set a more reasonable key-repeat rate
 xset r rate 200 50
 
-sleep 1
 # Run xmodmap after setting layout (disable caps-lock, add umlauts)
+# Twice, because it does not work consistently
+sleep 1
 xmodmap ~/.Xmodmap
 sleep 1
 xmodmap ~/.Xmodmap
