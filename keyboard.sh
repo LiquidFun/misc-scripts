40db0e060f63939b68ba5a1667bfdad748cdda55
Brutenis Gliwa <brutenis.gliwa@planet-ai.de>
2023-03-23 14:51:13 +0100

    Release keys on different hostnames

--- keyboard.sh
+++ keyboard.sh
@@ -2,7 +2,9 @@
 
 # Reset keys which might get stuck due to barrier issue:
 # https://github.com/debauchee/barrier/issues/207#issuecomment-542744072
-xdotool keyup Shift_L Shift_R Control_L Control_R Alt_L Alt_R Super_L Super_R Hyper_L Hyper_R Caps_Lock 204 205 206 207
+if [ "$HOSTNAME" -ne "arrakis" ]; then
+    xdotool keyup Shift_L Shift_R Control_L Control_R Alt_L Alt_R Super_L Super_R Hyper_L Hyper_R Caps_Lock 204 205 206 207
+do
 
 # Set us layout
 setxkbmap us
