b53ff7aaaa91407abde704a68c2b4fd8dc65c0b6
Brutenis Gliwa <brutenis.gliwa@planet-ai.de>
2023-03-23 15:24:50 +0100

    Fix if

--- keyboard.sh
+++ keyboard.sh
@@ -2,9 +2,9 @@
 
 # Reset keys which might get stuck due to barrier issue:
 # https://github.com/debauchee/barrier/issues/207#issuecomment-542744072
-if [ "$HOSTNAME" -ne "arrakis" ]; then
+if [[ "$HOSTNAME" -ne "arrakis" ]]; then
     xdotool keyup Shift_L Shift_R Control_L Control_R Alt_L Alt_R Super_L Super_R Hyper_L Hyper_R Caps_Lock 204 205 206 207
-do
+fi
 
 # Set us layout
 setxkbmap us
