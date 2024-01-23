aa82c8382329865a54acf348979a6e8afde717f8
Brutenis Gliwa <brutenis.gliwa@planet-ai.de>
2024-01-23 13:44:52 +0100

    Add foreach.py

--- keyboard.sh
+++ keyboard.sh
@@ -3,6 +3,7 @@
 # Reset keys which might get stuck due to barrier issue:
 # https://github.com/debauchee/barrier/issues/207#issuecomment-542744072
 if [[ "$HOSTNAME" -ne "arrakis" ]]; then
+    echo "keyup"
     xdotool keyup Shift_L Shift_R Control_L Control_R Alt_L Alt_R Super_L Super_R Hyper_L Hyper_R Caps_Lock 204 205 206 207
 fi
 
@@ -14,10 +15,8 @@ xset r rate 200 50
 
 # Run xmodmap after setting layout (disable caps-lock, add umlauts)
 # Twice, because it does not work consistently
-sleep 1
 xmodmap ~/.Xmodmap
-sleep 1
-xmodmap ~/.Xmodmap
+# xmodmap ~/.Xmodmap
 
 # Turn on numlock
 # numlockx on
